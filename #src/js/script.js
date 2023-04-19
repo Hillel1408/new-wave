47341a844b715fec94d572fba4a12e88f3f62ea7
Hillel1408 <etemax@bk.ru>
2023-04-19 13:14:08 +0300

    New-wave

--- #src/js/script.js
+++ #src/js/script.js
@@ -13,3 +13,21 @@ const reviewsSwiper = new Swiper('.reviews__swiper', {
         clickable: true,
     },
 });
+
+const certificatesSwiper = new Swiper('.slider__container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    freeMode: true,
+    spaceBetween: 88,
+    initialSlide: 2,
+    breakpoints: {
+        1160: {
+            slidesPerView: 2,
+        },
+    },
+    navigation: {
+        nextEl: '.slider__swiper-button-next',
+        prevEl: '.slider__swiper-button-prev',
+    },
+});
